7861ec9bc4ebe08341a9baf8001a7ea2ef2dd465
Tom Powell <zifnab@zifnab06.net>
2017-05-22 23:20:13 -0700

    add silly things

---
+++ plugins/silly.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import re
+import sys
+
+import requests
+
+from rtmbot.core import Plugin
+from plugins.db import DataStore
+
+
+class Silly(Plugin):
+    def process_message(self, data):
+        message = data['text']
+        if 'groot' in message.lower():
+            self.outputs.append([data['channel'], 'I AM GROOT')
